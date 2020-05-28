5eceeace80376a8f4791cce320e907ca30112922
balag_developer2 <balag.developer2@gmail.com>
2020-05-28 14:34:02 +0530

    Linting using ESLINT

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import numeral from 'numeral';
 import './index.css'
 
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I would pay ${courseValue} for this course`);
+console.log(`I would pay ${courseValue} for this course`); // eslint-disable-line no-console
