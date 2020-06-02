91b127d4a395192874fd196cf0e44d06d68763a7
balag_developer2 <balag.developer2@gmail.com>
2020-06-02 16:12:10 +0530

    Testing and Continuous integration initial commit

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,17 @@ app.get('/' , function(req,res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+
+app.get("/users", function(req,res){
+
+  res.json([
+    {"id":1, "firstName": "Bala", "lastName": "G", "email":"bala@gmail.com"},
+    {"id":2, "firstName": "Venkat", "lastName": "V", "email":"venkat@gmail.com"},
+    {"id":1, "firstName": "Rajesh", "lastName": "S", "email":"rajesh@gmail.com"}
+  ]);
+})
+
+
 app.listen(port, function(err) {
   if(err) {
     console.log(err);
